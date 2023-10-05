44d6ec3fdb6a5e7d49207456d5df3e443d0dacf8
Sergey E <evilx.emelyanov@gmail.com>
2023-10-05 20:52:14 +0300

    fixed true in switch/case statement

--- js/main.js
+++ js/main.js
@@ -1,16 +1,15 @@
 let numOrStr = prompt('input number or string');
 console.log(numOrStr);
 
-switch (true) {
-    case numOrStr === null:
-        console.log('ви скасували')
+switch (numOrStr) {
+    case null:
+        console.log('ви скасували');
         break;
-    case numOrStr.trim() === '':
+    case '':
         console.log('Empty String');
         break;
-    case isNaN(+numOrStr):
-        console.log('number is Ba_NaN');
-        break;
     default:
-        console.log('OK!');
+        console.log(
+            isNaN(+numOrStr) ? 'number is Ba_NaN' : 'OK!'
+        );
 }
